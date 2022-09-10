b0673d9afd8d0fced9665fc5b333875ef91f7e12
WayTo1Million <98961337+WayTo1Million@users.noreply.github.com>
2022-09-10 22:46:54 +0200

    Delete index.js

--- pages/index.js
+++
@@ -1,1 +0,0 @@
-<iframe data-aa='2079188' loading='lazy' src='//acceptable.a-ads.com/2079188' style='border:0px; padding:0; width:100%; height:100%; overflow:hidden; background-color: transparent;'></iframe>
